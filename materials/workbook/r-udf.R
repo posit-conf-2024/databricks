195a7189d627a5d32dbc2487a6ec5c54e17a200b
Edgar Ruiz <edgararuiz@gmail.com>
2024-06-21 13:41:56 -0500

    Adds vetiver steps

--- materials/workbook/r-udf.R
+++ materials/workbook/r-udf.R
@@ -15,13 +15,8 @@ lendingclub_prep <- lendingclub_sample |>
 
 library(tidymodels)
 
-lendingclub_rec <- recipe(
-  int_rate ~ ., 
-  data = lendingclub_prep
-  ) |> 
-  step_mutate(
-    term = trimws(substr(term, 1,4))
-  ) |> 
+lendingclub_rec <- recipe(int_rate ~ ., data = lendingclub_prep) |> 
+  step_mutate(term = trimws(substr(term, 1,4))) |> 
   step_mutate(across(everything(), as.numeric)) |> 
   step_normalize(all_numeric_predictors()) |>
   step_impute_mean(all_of(c("bc_open_to_buy", "bc_util"))) |>   
@@ -43,12 +38,41 @@ lendingclub_fit |>
   metrics(int_rate, .pred)
 
 library(ggplot2)
+
 predict(lendingclub_fit, lendingclub_sample) |> 
   ggplot() +
   geom_histogram(aes(.pred))
 
+library(vetiver)
+library(pins)
 
+v <- vetiver_model(lendingclub_fit, "lendingclub_model")
+
+board <- board_connect(
+  auth = "manual",
+  server = Sys.getenv("CONNECT_SERVER"),
+  key = Sys.getenv("CONNECT_API_KEY")
+)
+
+board |> 
+  vetiver_pin_write(v)
+
+predict_vetiver <- function(x) {
+  library(workflows)
+  board <- pins::board_connect(
+    auth = "manual", 
+    server = "https://connect.posit.it/",
+    key = "Ag32KJvRRjhPHYJhWAIiZtSp9OnLbcJB"
+  )
+  model <- vetiver::vetiver_pin_read(board, "edgar/lendingclub_model")
+  preds <- predict(model, x)
+  x$pred <- preds[,1][[1]]
+  x[x$pred >= 20, ]
+}
+
+predict_vetiver(lendingclub_prep)
 
 lendingclub_dat |> 
+  select(int_rate, term, bc_util, bc_open_to_buy, all_util) |> 
   head() |> 
-  spark_apply(lendingclub_predict) 
+  spark_apply(predict_vetiver) 
