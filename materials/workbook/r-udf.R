0b43e8bd8560137ed9609650879974b064ef3cb9
Edgar Ruiz <edgararuiz@gmail.com>
2024-06-21 11:32:32 -0500

    Moves select

--- materials/workbook/r-udf.R
+++ materials/workbook/r-udf.R
@@ -3,27 +3,16 @@ library(dplyr)
 sc <- spark_connect(method = "databricks_connect")
 lendingclub_dat <- tbl(sc, dbplyr::in_catalog("hive_metastore", "default", "lendingclub"))
 
-
 lendingclub_sample <- lendingclub_dat |> 
   slice_sample(n = 1000) |> 
   collect()
 
-
 library(tidymodels)
 
-lendingclub_prep <- lendingclub_sample |> 
-  select(int_rate, term, bc_util, bc_open_to_buy, all_util) |> 
-  mutate(
-    int_rate = substr(int_rate, 1, nchar(int_rate) - 1),
-    term = trimws(substr(term, 1,4))
-    ) |> 
-  mutate(across(everything(), as.numeric)) |> 
-  filter(!if_any(everything(), is.na)) 
-
-rec_lendingclub <- recipe(int_rate ~ ., data = lendingclub_sample) |> 
+lendingclub_rec <- recipe(int_rate ~ ., data = lendingclub_sample) |> 
   step_select(int_rate, term, bc_util, bc_open_to_buy, all_util) |> 
   step_mutate(
-    #int_rate = substr(int_rate, 1, nchar(int_rate) - 1),
+    int_rate = as.numeric(stringr::str_remove(int_rate, "%")),
     term = trimws(substr(term, 1,4))
   ) |> 
   step_mutate(across(everything(), as.numeric)) |> 
@@ -31,11 +20,14 @@ rec_lendingclub <- recipe(int_rate ~ ., data = lendingclub_sample) |>
   step_impute_mean(all_of(c("bc_open_to_buy", "bc_util"))) |>   
   step_filter(!if_any(everything(), is.na)) 
 
-linear_lendingclub <- linear_reg()
+lendingclub_lr <- linear_reg()
 
-workflow_lendingclub <- workflow() |> 
-  add_model(linear_lendingclub) |> 
-  add_recipe(rec_lendingclub) 
+lendingclub_wf <- workflow() |> 
+  add_model(lendingclub_lr) |> 
+  add_recipe(lendingclub_rec) 
 
-fit_lendingclub <- workflow_lendingclub |> 
+lendingclub_fit <- lendingclub_wf |> 
   fit(data = lendingclub_sample)
+
+
+
