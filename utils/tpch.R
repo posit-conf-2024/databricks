4ab35e9bbf48222ef81ad7c7929b1849d1d5604f
Edgar Ruiz <edgararuiz@gmail.com>
2024-07-26 14:20:32 -0500

    Adds lineitem download and processing

--- utils/tpch.R
+++ utils/tpch.R
@@ -20,4 +20,22 @@ read_delim(
                 "c_phone", "c_acctbal", "c_mktsegment","c_comment") 
   ) |> 
   select(1:8) |> 
-  write_csv(path(tcph_path, "csv", "customer.csv")  )
+  write_csv(path(tcph_path, "csv", "customer.csv"))
+
+
+file_lineitem <- path(tcph_path, "tbls", "lineitem.tbl.gz")  
+if(!file_exists(file_lineitem)) {
+  download.file(path(files_url, "lineitem.tbl.gz"), file_lineitem)
+}
+
+read_delim(
+  file = file_lineitem,
+  delim = "|", 
+  col_names = c("l_orderkey", "l_partkey", "l_suppkey", "l_linenumber", "l_quantity", "l_extendedprice", "l_discount", "l_tax", "l_returnflag", "l_linestatus", "l_shipdate", "l_commitdate", "l_receiptdate", "l_shipinstruct", "l_shipmode", "l_comment") 
+) |> 
+  select(1:16) |> 
+  write_csv(path(tcph_path, "csv", "lineitem.csv"))
+
+
+
+
