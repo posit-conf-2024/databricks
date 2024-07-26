322b39496a5986c923472ce069654c110d058324
Edgar Ruiz <edgararuiz@gmail.com>
2024-07-26 15:05:10 -0500

    Avoids code duplication by packing process in a function

--- utils/tpch.R
+++ utils/tpch.R
@@ -1,41 +1,43 @@
 library(tidyverse)
+library(arrow)
 library(fs)
 
-tcph_path <- path("utils/tpch_files")
-if(!dir_exists(tcph_path)) dir_create(tcph_path)
-if(!dir_exists(path(tcph_path, "tbls"))) dir_create(path(tcph_path, "tbls"))
-if(!dir_exists(path(tcph_path, "csv"))) dir_create(path(tcph_path, "csv"))
+process_tcph_file <- function(x, col_names) {
+  tcph_path <- path("utils/tpch_files")
+  if (!dir_exists(tcph_path)) dir_create(tcph_path)
+  if (!dir_exists(path(tcph_path, "tbls"))) dir_create(path(tcph_path, "tbls"))
+  if (!dir_exists(path(tcph_path, "parquet"))) dir_create(path(tcph_path, "parquet"))
 
-files_url <- "https://github.com/aleaugustoplus/tpch-data/raw/master/"
+  files_url <- "https://github.com/aleaugustoplus/tpch-data/raw/master/"
 
-file_customer <- path(tcph_path, "tbls", "customer.tbl.gz")  
-if(!file_exists(file_customer)) {
-  download.file(path(files_url, "customer.tbl.gz"), file_customer)
+  file_path <- path(tcph_path, "tbls", paste0(x, ".tbl.gz"))
+  if (!file_exists(file_path)) {
+    download.file(path(files_url, paste0(x, ".tbl.gz")), file_path)
+  }
+
+  read_delim(
+    file = file_path,
+    delim = "|",
+    col_names = col_names
+  ) |>
+    select(1:length(col_names)) |>
+    write_parquet(path(tcph_path, "parquet", x, ext = "parquet"))
 }
 
-read_delim(
-  file = file_customer,
-  delim = "|", 
-  col_names = c("c_custkey", "c_name", "c_address","c_nationkey", 
-                "c_phone", "c_acctbal", "c_mktsegment","c_comment") 
-  ) |> 
-  select(1:8) |> 
-  write_csv(path(tcph_path, "csv", "customer.csv"))
+process_tcph_file(
+  x = "customer",
+  col_names = c(
+    "c_custkey", "c_name", "c_address", "c_nationkey",
+    "c_phone", "c_acctbal", "c_mktsegment", "c_comment"
+  )
+)
 
-
-file_lineitem <- path(tcph_path, "tbls", "lineitem.tbl.gz")  
-if(!file_exists(file_lineitem)) {
-  download.file(path(files_url, "lineitem.tbl.gz"), file_lineitem)
-}
-
-read_delim(
-  file = file_lineitem,
-  delim = "|", 
-  col_names = c("l_orderkey", "l_partkey", "l_suppkey", "l_linenumber", "l_quantity", "l_extendedprice", "l_discount", "l_tax", "l_returnflag", "l_linestatus", "l_shipdate", "l_commitdate", "l_receiptdate", "l_shipinstruct", "l_shipmode", "l_comment") 
-) |> 
-  select(1:16) |> 
-  write_csv(path(tcph_path, "csv", "lineitem.csv"))
-
-
-
-
+process_tcph_file(
+  x = "lineitem",
+  col_names = c(
+    "l_orderkey", "l_partkey", "l_suppkey", "l_linenumber",
+    "l_quantity", "l_extendedprice", "l_discount", "l_tax",
+    "l_returnflag", "l_linestatus", "l_shipdate", "l_commitdate",
+    "l_receiptdate", "l_shipinstruct", "l_shipmode", "l_comment"
+  )
+)
