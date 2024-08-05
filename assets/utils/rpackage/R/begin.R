9d4e525966f11ed2e3d702bec0f5b179190b0dd7
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-05 07:52:47 -0500

    Adds token routines

--- assets/utils/rpackage/R/begin.R
+++ assets/utils/rpackage/R/begin.R
@@ -13,7 +13,20 @@ begin <- function() {
     "DATABRICKS_HOST",
     "rstudio-partner-posit-default.cloud.databricks.com"
     )
-  env_file <- entry_environ(env_file, "DATABRICKS_TOKEN", "token")
+  token_val <- "{{db-token}}"
+  db_token <- "DATABRICKS_TOKEN"
+  env_file <- entry_environ(env_file, db_token, token_val)
+  if(any(token_val == token_val)) {
+    pwd <- readline(prompt = "- Enter password: ")
+    token <- get_token(pwd)
+    token_line <- env_file == paste0(db_token, "=", token_val)
+    if(!is.null(token)) {
+      env_file[token_line] <- paste0(db_token, "=", token)
+    } else {
+      env_file[token_line] <- NULL
+    }
+  }
+  cli_alert_info("Writing .Renviron file")
   writeLines(env_file, r_environ)
 
   source_proj <- path("/databricks")
@@ -23,9 +36,20 @@ begin <- function() {
   }
 }
 
+get_token <- function(x) {
+  ret <- NULL
+  if(x == "abc") {
+    ret <- "ACTUAL TOKEN"
+    cli_alert_info("Personal Access Token retrieved")
+  } else {
+    cli_alert_warning("Incorrect password")
+  }
+  ret
+}
+
 entry_environ <- function(x, entry, value) {
   found_entry <- substr(x, 1, nchar(entry)) == entry
-  if(sum(found_entry) == 0) {
+  if(!any(found_entry)) {
     x <- c(x, paste0(entry, "=", value))
     cli_alert_info(c("Adding ", entry, " to .Renviron file" ))
   } else {
