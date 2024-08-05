307e5b00b02c4a1df5a855e5bad45397736d44aa
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-05 09:38:40 -0500

    Creates API to get token

---
+++ db-token/plumber.R
@@ -0,0 +1,20 @@
+library(plumber)
+
+#* @apiTitle Databricks PAT
+#* @apiDescription Returns PAT to use during workshop
+
+#* Echo back the input
+#* @param pwd The pwd to access the token
+#* @get /echo
+function(pwd = "") {
+  ret <- ""
+  expected_pwd <- Sys.getenv("API_PASSWORD", unset = NA)
+  current_token <- Sys.getenv("CURRENT_TOKEN", unset = NA)
+  if(!is.na(expected_pwd) && !is.na(current_token)) {
+    if(pwd == expected_pwd) {
+      ret <- current_token
+    }    
+  }
+  ret
+}
+
